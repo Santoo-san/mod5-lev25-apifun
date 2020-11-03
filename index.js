ea7a43562e0a23124dda4df04beb14916173c805
Hans Kool <Hansjekool@hotmail.com>
2020-11-03 15:04:02 +0100

    3-11 apifun

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 console.log("beginning");
 
-const showData = async () => {
-const data = await getData()
-console.log("show me the data: ", data);
+// general
+const removeAll = () => {
+  const allListItems = document.getElementById("result-list");
+  allListItems.querySelectorAll("*").forEach((n) => n.remove());
 };
 
-showData();
+document
+  .getElementById("remove-all-button")
+  .addEventListener("click", removeAll);
 
-console.log("The End");
+const emptyResultList = () =>
+  (document.getElementById("result-list").innerHTML = "");
+
+document.getElementById("getJoke").addEventListener("click", getTextJoke);
+
+document.getElementById("showJoke").addEventListener("click", getImgJoke);
+
+// showData();
+
+const addTekstJokeToDom = (joke) => {
+  const listItem = document.createElement("li");
+  listItem.appendChild(document.createTextNode(joke));
+  const list = document.getElementById("result-list");
+  list.appendChild(listItem);
+};
+
+const addImgJokeToDom = (url => {
+  const listItem = document.createElement(url); //ik verwacht een complete url + img tags terug zoals : <img src="https://icanhazdadjoke.com/j/R7UfaahVfFd.png" />
+  // listItem.appendChild(document.createTextNode(`hallo`));
+  // listItem.appendChild(url);
+  // const listImg = document.createElement("IMG");
+  // listImg.src = url;
+  const list = document.getElementById("result-list");
+  list.appendChild(listItem);
+  // listItem.appendChild(listImg);
+};
+
+// //'reservecode'
+// const addImgJokeToDom = (url) => {
+//   const listItem = document.createElement("li");
+//   listItem.appendChild(document.createTextNode(`hallo`));
+//   const listImg = document.createElement("IMG");
+//   listImg.src = url;
+//   const list = document.getElementById("result-list");
+//   list.appendChild(listItem);
+//   listItem.appendChild(listImg);
+// };
