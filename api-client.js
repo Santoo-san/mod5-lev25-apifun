ee93c4d00afad1a78f202859b6d05da75377af7c
Hans Kool <Hansjekool@hotmail.com>
2020-11-03 16:18:00 +0100

    final

--- api-client.js
+++ api-client.js
@@ -1,7 +1,6 @@
 console.log("beginning API");
 
 //tekst joke
-
 const myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
 myHeaders.append(
@@ -23,24 +22,10 @@ const getTextJoke = async () => {
 };
 
 //image joke
-
-const myHeadersImg = new Headers();
-myHeadersImg.append(
-  "Cookie",
-  "__cfduid=dcabffc78b05d2d31ebdeecd6642c28f01604056242"
-);
-
-const requestOptionsImg = {
-  method: "GET",
-  headers: myHeadersImg,
-  redirect: "follow",
-  mode: "no-cors",
-};
-
 const getImgJoke = async () => {
-  fetch("https://icanhazdadjoke.com/j/08xHQCdx5Ed.png", requestOptionsImg)
+  fetch("https://icanhazdadjoke.com/", requestOptions)
     .then((response) => response.json())
-    .then((result) => addImgJokeToDom(result))
+    .then((result) => addImgJokeToDom(result.id))
     .catch((error) => console.log("error", error));
 };
 
--- index.js
+++ index.js
@@ -18,8 +18,8 @@ document.getElementById("getJoke").addEventListener("click", getTextJoke);
 document.getElementById("showJoke").addEventListener("click", getImgJoke);
 
 // showData();
-
 const addTekstJokeToDom = (joke) => {
+  emptyResultList();
   const listItem = document.createElement("li");
   listItem.appendChild(document.createTextNode(joke));
   const list = document.getElementById("result-list");
@@ -27,23 +27,9 @@ const addTekstJokeToDom = (joke) => {
 };
 
 const addImgJokeToDom = (url) => {
-  const listItem = document.createElement(url); //ik verwacht een complete url + img tags terug zoals : <img src="https://icanhazdadjoke.com/j/R7UfaahVfFd.png" />
-  // listItem.appendChild(document.createTextNode(`hallo`));
-  // listItem.appendChild(url);
-  // const listImg = document.createElement("IMG");
-  // listImg.src = url;
+  emptyResultList();
+  const listImg = document.createElement("IMG");
+  listImg.src = `https://icanhazdadjoke.com/j/${url}.png`;
   const list = document.getElementById("result-list");
-  list.appendChild(listItem);
-  // listItem.appendChild(listImg);
+  list.appendChild(listImg);
 };
-
-// //'reservecode'
-// const addImgJokeToDom = (url) => {
-//   const listItem = document.createElement("li");
-//   listItem.appendChild(document.createTextNode(`hallo`));
-//   const listImg = document.createElement("IMG");
-//   listImg.src = url;
-//   const list = document.getElementById("result-list");
-//   list.appendChild(listItem);
-//   listItem.appendChild(listImg);
-// };
