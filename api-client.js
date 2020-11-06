cf2a7c760ece56b6b326a67929483a776c5045db
Hans Kool <Hansjekool@hotmail.com>
2020-11-06 11:24:29 +0100

    lev5 api eindopdracht FINAL

--- api-client.js
+++ api-client.js
@@ -1,32 +1,47 @@
-console.log("beginning API");
-
-//tekst joke
-const myHeaders = new Headers();
-myHeaders.append("Accept", "application/json");
-myHeaders.append(
+//general API settings
+const headersJsonbox = new Headers();
+headersJsonbox.append(
   "Cookie",
-  "__cfduid=dcabffc78b05d2d31ebdeecd6642c28f01604056242"
+  "__cfduid=d0268583f798bb90d5da418f860219d2f1604486333"
 );
 
-const requestOptions = {
+const requestOptionsJsonbox = {
   method: "GET",
-  headers: myHeaders,
+  headers: headersJsonbox,
   redirect: "follow",
 };
 
-const getTextJoke = async () => {
-  fetch("https://icanhazdadjoke.com/", requestOptions)
+const getLimit = "?limit=1000";
+const baseUrl = "https://jsonbox.io/box_7dbe7156883cf09b09fe";
+const getUrl = `https://jsonbox.io/box_7dbe7156883cf09b09fe${getLimit}`;
+
+//get request
+const getArrayJsonbox = async () => {
+  fetch(getUrl, requestOptionsJsonbox)
     .then((response) => response.json())
-    .then((result) => addTekstJokeToDom(result.joke))
+    .then((result) => addToDoListToDom(result))
     .catch((error) => console.log("error", error));
 };
 
-//image joke
-const getImgJoke = async () => {
-  fetch("https://icanhazdadjoke.com/", requestOptions)
-    .then((response) => response.json())
-    .then((result) => addImgJokeToDom(result.id))
-    .catch((error) => console.log("error", error));
+//post request
+const postArrayJsonbox = async (data) => {
+  await fetch(baseUrl, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  await getArrayJsonbox().catch((error) => console.log("error", error));
 };
 
-console.log("The End API");
+//delete request
+const deleteJsonbox = async (ID) => {
+  await fetch(`${baseUrl}/${ID}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  await getArrayJsonbox().catch((error) => console.log("error", error));
+};
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("beginning");
-
 // general
 const removeAll = () => {
   const allListItems = document.getElementById("result-list");
@@ -13,23 +11,43 @@ document
 const emptyResultList = () =>
   (document.getElementById("result-list").innerHTML = "");
 
-document.getElementById("getJoke").addEventListener("click", getTextJoke);
+document
+  .getElementById("getToDolist")
+  .addEventListener("click", getArrayJsonbox);
 
-document.getElementById("showJoke").addEventListener("click", getImgJoke);
+//inputfield
+const inputField = document.getElementById("addToDoList");
+const submitButton = document.getElementById("submitButton");
+
+const getButtonClick = submitButton.addEventListener("click", () => {
+  const value = inputField.value;
+  postArrayJsonbox({ description: value, done: false });
+  inputField.value = "";
+});
 
 // showData();
-const addTekstJokeToDom = (joke) => {
-  emptyResultList();
+const createListitem = (item, list) => {
   const listItem = document.createElement("li");
-  listItem.appendChild(document.createTextNode(joke));
-  const list = document.getElementById("result-list");
+  listItem.className = "ToDoItem";
+  listItem.appendChild(document.createTextNode(`${item.description} `));
   list.appendChild(listItem);
+  addIconToListitem(item, listItem);
 };
 
-const addImgJokeToDom = (url) => {
+const addIconToListitem = (item, listItem) => {
+  const listIcon = document.createElement("i");
+  listIcon.id = item._id;
+  listIcon.className = "far fa-trash-alt deleteButton";
+  listIcon.addEventListener("click", () => {
+    deleteJsonbox(listIcon.id);
+  });
+  listItem.appendChild(listIcon);
+};
+
+const addToDoListToDom = (result) => {
   emptyResultList();
-  const listImg = document.createElement("IMG");
-  listImg.src = `https://icanhazdadjoke.com/j/${url}.png`;
-  const list = document.getElementById("result-list");
-  list.appendChild(listImg);
+  result.forEach((item) => {
+    const list = document.getElementById("result-list");
+    createListitem(item, list);
+  });
 };
